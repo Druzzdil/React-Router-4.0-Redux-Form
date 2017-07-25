1e7efe7d1c218a13d8db8151eaf0f8b98a5bca8e
Druzzdil <staniszewski86@gmail.com>
2017-07-25 21:34:49 +0200

    basics of router 4

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Main extends React.Component {
+  render() { return (<h1>Main</h1>)}
+}
 
 
 class First extends React.Component {
@@ -20,6 +24,11 @@ class Second extends React.Component {
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <First />
+   <BrowserRouter>
+      <div>
+        <Route path="/first" component={First}/>
+        <Route path="/second" component={Second}/>
+      </div>
+   </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
