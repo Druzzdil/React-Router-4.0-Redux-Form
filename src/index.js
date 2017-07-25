2ac362fb77d52f81d21e5cb257e54d2e4b0ecd9a
Druzzdil <staniszewski86@gmail.com>
2017-07-25 21:20:05 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import {} from 'react-router-dom';
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+
+class First extends React.Component {
+  render() { return (<h1>hey dude !</h1>)}
+}
+
+class Second extends React.Component {
+  render() { return (<h1>farewell to the moon !</h1>)}
+}
+
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <First />
   </Provider>
   , document.querySelector('.container'));
