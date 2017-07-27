3c7971038b5c5c8921d51de46de1afe7bb969aaa
Druzzdil <staniszewski86@gmail.com>
2017-07-27 14:44:50 +0200

    add first redux input field

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -3,27 +3,36 @@ import { connect } from "react-redux";
 import { fetchPosts } from '../actions/index';
 import _ from 'lodash';
 import { Field, reduxForm } from 'redux-form'
-
+// const { DOM: { input, select, textarea } } = React
 //redux-form
 import { createStore, combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
  class PostNew extends Component {
 
-  constructor(props){
-    super(props);
-  }
+
+renderTitleField(field){
+    return (
+      <div> 
+        <input
+          tpe="text"
+          {...field.input}
+        
+        />
+      
+      </div>
+    )
+}
+
 
   render() {
     return (
       <form>
         <Field 
           name="title" 
-          component={}
-          
-          
-          />
-         
+          component={this.renderTitleField}
+        />
+
       </form>
     );
   }
